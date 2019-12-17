1d6b82c035b7d0f90c898134a625c96ff0f7b146
BillZong <billzong@163.com>
2019-12-17 17:55:47 +0800

    simplify hostname and network name replacement

--- network/populate_hostname.sh
+++ network/populate_hostname.sh
@@ -14,121 +14,115 @@ elif [ "$ARCH" == "Darwin" ]; then
   FLAG="-it"
 fi
 
+# Replace all network names
+replaceNetworkName() {
+    if [ $# -lt 3 ]; then
+        echo "Usage: replaceNetworkName file_path oldname newname"
+        exit 1
+    fi
+
+    # TODO: have to be more specific to roll back
+    sed "$FLAG" "s/$2/$3/g" $1
+}
+sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $ZK_COMPOSE_PATH
+sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $KAFKA_COMPOSE_PATH # network
+
+
 # Zookeeper + Kafka
 sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $ZK_COMPOSE_PATH
 sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $KAFKA_COMPOSE_PATH
+# # network of ZK and Kafka
+replaceNetworkName $ZK_COMPOSE_PATH $OLD_NETWORK_NAME $NETWORK_NAME
+replaceNetworkName $KAFKA_COMPOSE_PATH $OLD_NETWORK_NAME $NETWORK_NAME
 
-# ORG1
-ORG1_CA_PATH=$(ls $VOLUMES_DIR/certs/crypto-config/peerOrganizations/org1.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $ORDERER0_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $PEER_ORG1_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $SERVICE_ORG1_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG1_CA_PATH#g" $SERVICE_ORG1_COMPOSE_PATH
-# # replace directories
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $ORDERER0_COMPOSE_PATH
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $PEER_ORG1_COMPOSE_PATH
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $SERVICE_ORG1_COMPOSE_PATH
-sed "$FLAG" "s#- .*/chaincode:#- $VOLUMES_DIR/chaincode:#g" $SERVICE_ORG1_COMPOSE_PATH
-sed "$FLAG" "s#- .*/fabric-src/#- $VOLUMES_DIR/fabric-src/#g" $SERVICE_ORG1_COMPOSE_PATH
-sed "$FLAG" "s#- .*/bin/:#- $VOLUMES_DIR/bin/:#g" $SERVICE_ORG1_COMPOSE_PATH
+replaceOrdererCompose() {
+    if [ $# -lt 2 ]; then
+        echo "Usage: replaceOrgFunc hostname orderer_compose_path"
+        exit 1
+    fi
 
-# ORG2
-ORG2_CA_PATH=$(ls $VOLUMES_DIR/certs/crypto-config/peerOrganizations/org2.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG2_HOSTNAME/g" $ORDERER1_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG2_HOSTNAME/g" $PEER_ORG2_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG2_HOSTNAME/g" $SERVICE_ORG2_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG2_CA_PATH#g" $SERVICE_ORG2_COMPOSE_PATH
-# # replace directories
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $ORDERER1_COMPOSE_PATH
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $PEER_ORG2_COMPOSE_PATH
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $SERVICE_ORG2_COMPOSE_PATH
-sed "$FLAG" "s#- .*/chaincode:#- $VOLUMES_DIR/chaincode:#g" $SERVICE_ORG2_COMPOSE_PATH
-sed "$FLAG" "s#- .*/fabric-src/#- $VOLUMES_DIR/fabric-src/#g" $SERVICE_ORG2_COMPOSE_PATH
-sed "$FLAG" "s#- .*/bin/:#- $VOLUMES_DIR/bin/:#g" $SERVICE_ORG2_COMPOSE_PATH
+    local HOSTNAME=$1
+    local ORDERER_COMPOSE_PATH=$2
 
-# ORG3
-ORG3_CA_PATH=$(ls $VOLUMES_DIR/certs/crypto-config/peerOrganizations/org3.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG3_HOSTNAME/g" $ORDERER2_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG3_HOSTNAME/g" $PEER_ORG3_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG3_HOSTNAME/g" $SERVICE_ORG3_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG3_CA_PATH#g" $SERVICE_ORG3_COMPOSE_PATH
-# # replace directories
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $ORDERER2_COMPOSE_PATH
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $PEER_ORG3_COMPOSE_PATH
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $SERVICE_ORG3_COMPOSE_PATH
-sed "$FLAG" "s#- .*/chaincode:#- $VOLUMES_DIR/chaincode:#g" $SERVICE_ORG3_COMPOSE_PATH
-sed "$FLAG" "s#- .*/fabric-src/#- $VOLUMES_DIR/fabric-src/#g" $SERVICE_ORG3_COMPOSE_PATH
-sed "$FLAG" "s#- .*/bin/:#- $VOLUMES_DIR/bin/:#g" $SERVICE_ORG3_COMPOSE_PATH
+    if [ -n "$ORDERER_COMPOSE_PATH" ]; then
+        sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $HOSTNAME/g" $ORDERER_COMPOSE_PATH
+        sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $ORDERER_COMPOSE_PATH
+    fi
+}
 
-# ORG4
-ORG4_CA_PATH=$(ls $VOLUMES_DIR/certs/crypto-config/peerOrganizations/org4.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG4_HOSTNAME/g" $PEER_ORG4_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG4_HOSTNAME/g" $SERVICE_ORG4_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG4_CA_PATH#g" $SERVICE_ORG4_COMPOSE_PATH
-# # replace directories
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $PEER_ORG4_COMPOSE_PATH
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $SERVICE_ORG4_COMPOSE_PATH
-sed "$FLAG" "s#- .*/chaincode:#- $VOLUMES_DIR/chaincode:#g" $SERVICE_ORG4_COMPOSE_PATH
-sed "$FLAG" "s#- .*/fabric-src/#- $VOLUMES_DIR/fabric-src/#g" $SERVICE_ORG4_COMPOSE_PATH
-sed "$FLAG" "s#- .*/bin/:#- $VOLUMES_DIR/bin/:#g" $SERVICE_ORG4_COMPOSE_PATH
+replacePeerCompose() {
+    if [ $# -lt 2 ]; then
+        echo "Usage: replaceOrgFunc hostname peer_org_compose_path"
+        exit 1
+    fi
 
-# ORG5
-ORG5_CA_PATH=$(ls $VOLUMES_DIR/certs/crypto-config/peerOrganizations/org5.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG5_HOSTNAME/g" $PEER_ORG5_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG5_HOSTNAME/g" $SERVICE_ORG5_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG5_CA_PATH#g" $SERVICE_ORG5_COMPOSE_PATH
-# # replace directories
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $PEER_ORG5_COMPOSE_PATH
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $SERVICE_ORG5_COMPOSE_PATH
-sed "$FLAG" "s#- .*/chaincode:#- $VOLUMES_DIR/chaincode:#g" $SERVICE_ORG5_COMPOSE_PATH
-sed "$FLAG" "s#- .*/fabric-src/#- $VOLUMES_DIR/fabric-src/#g" $SERVICE_ORG5_COMPOSE_PATH
-sed "$FLAG" "s#- .*/bin/:#- $VOLUMES_DIR/bin/:#g" $SERVICE_ORG5_COMPOSE_PATH
+    local HOSTNAME=$1
+    local PEER_ORG_COMPOSE_PATH=$2
 
-# ORG6
-ORG6_CA_PATH=$(ls $VOLUMES_DIR/certs/crypto-config/peerOrganizations/org6.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG6_HOSTNAME/g" $PEER_ORG6_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG6_HOSTNAME/g" $SERVICE_ORG6_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG6_CA_PATH#g" $SERVICE_ORG6_COMPOSE_PATH
-# # replace directories
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $PEER_ORG6_COMPOSE_PATH
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $SERVICE_ORG6_COMPOSE_PATH
-sed "$FLAG" "s#- .*/chaincode:#- $VOLUMES_DIR/chaincode:#g" $SERVICE_ORG6_COMPOSE_PATH
-sed "$FLAG" "s#- .*/fabric-src/#- $VOLUMES_DIR/fabric-src/#g" $SERVICE_ORG6_COMPOSE_PATH
-sed "$FLAG" "s#- .*/bin/:#- $VOLUMES_DIR/bin/:#g" $SERVICE_ORG6_COMPOSE_PATH
+    if [ -n "$PEER_ORG_COMPOSE_PATH" ]; then
+        sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $HOSTNAME/g" $PEER_ORG_COMPOSE_PATH
+        sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $PEER_ORG_COMPOSE_PATH
+    fi
+}
 
-# ORG7
-ORG7_CA_PATH=$(ls $VOLUMES_DIR/certs/crypto-config/peerOrganizations/org7.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG7_HOSTNAME/g" $PEER_ORG7_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG7_HOSTNAME/g" $SERVICE_ORG7_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG7_CA_PATH#g" $SERVICE_ORG7_COMPOSE_PATH
-# # replace directories
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $PEER_ORG7_COMPOSE_PATH
-sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $SERVICE_ORG7_COMPOSE_PATH
-sed "$FLAG" "s#- .*/chaincode:#- $VOLUMES_DIR/chaincode:#g" $SERVICE_ORG7_COMPOSE_PATH
-sed "$FLAG" "s#- .*/fabric-src/#- $VOLUMES_DIR/fabric-src/#g" $SERVICE_ORG7_COMPOSE_PATH
-sed "$FLAG" "s#- .*/bin/:#- $VOLUMES_DIR/bin/:#g" $SERVICE_ORG7_COMPOSE_PATH
+replaceServiceCompose() {
+    if [ $# -lt 2 ]; then
+        echo "Usage: replaceOrgFunc hostname service_org_compose_path"
+        exit 1
+    fi
 
-# Replace all network names
-# TODO: have to be more specific to roll back
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $KAFKA_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $ZK_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $ORDERER0_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $ORDERER1_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $ORDERER2_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $PEER_ORG1_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $PEER_ORG2_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $PEER_ORG3_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $PEER_ORG4_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $PEER_ORG5_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $PEER_ORG6_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $PEER_ORG7_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $SERVICE_ORG1_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $SERVICE_ORG2_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $SERVICE_ORG3_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $SERVICE_ORG4_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $SERVICE_ORG5_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $SERVICE_ORG6_COMPOSE_PATH
-sed "$FLAG" "s/skcript/$NETWORK_NAME/g" $SERVICE_ORG7_COMPOSE_PATH
+    local HOSTNAME=$1
+    local SERVICE_ORG_COMPOSE_PATH=$2
+
+    if [ -n "$SERVICE_ORG_COMPOSE_PATH" ]; then
+        ORG_CA_PATH=$(ls $VOLUMES_DIR/certs/crypto-config/peerOrganizations/org$INDEX.example.com/ca/ | grep "_sk")
+        sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $HOSTNAME/g" $SERVICE_ORG_COMPOSE_PATH
+        sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG_CA_PATH#g" $SERVICE_ORG_COMPOSE_PATH
+
+        sed "$FLAG" "s#- .*/certs#- $VOLUMES_DIR/certs#g" $SERVICE_ORG_COMPOSE_PATH
+        sed "$FLAG" "s#- .*/chaincode:#- $VOLUMES_DIR/chaincode:#g" $SERVICE_ORG_COMPOSE_PATH
+        sed "$FLAG" "s#- .*/fabric-src/#- $VOLUMES_DIR/fabric-src/#g" $SERVICE_ORG_COMPOSE_PATH
+        sed "$FLAG" "s#- .*/bin/:#- $VOLUMES_DIR/bin/:#g" $SERVICE_ORG_COMPOSE_PATH
+    fi
+}
+
+replaceOrgFunc() {
+    if [ $# -lt 2 ]; then
+        echo "Usage: replaceOrgFunc hostname [service_org_compose_path] [peer_org_compose_path]  [orderer_compose_path]"
+        exit 1
+    fi
+
+    local HOSTNAME=$1
+    local SERVICE_ORG_COMPOSE_PATH=$2
+    local PEER_ORG_COMPOSE_PATH=$3
+    local ORDERER_COMPOSE_PATH=$4
+
+    if [ -n "$ORDERER_COMPOSE_PATH" ]; then
+        replaceOrdererCompose $HOSTNAME $ORDERER_COMPOSE_PATH
+    fi
+    if [ -n "$PEER_ORG_COMPOSE_PATH" ]; then
+        replacePeerCompose $HOSTNAME $PEER_ORG_COMPOSE_PATH
+    fi
+    if [ -n "$SERVICE_ORG_COMPOSE_PATH" ]; then
+        replaceServiceCompose $HOSTNAME $SERVICE_ORG_COMPOSE_PATH
+    fi
+}
+
+# Org1
+replaceOrgFunc ${ORG1_HOSTNAME} ${SERVICE_ORG1_COMPOSE_PATH} ${PEER_ORG1_COMPOSE_PATH} ${ORDERER0_COMPOSE_PATH}
+replaceNetworkName $SERVICE_ORG1_COMPOSE_PATH $OLD_NETWORK_NAME $NETWORK_NAME
+replaceNetworkName $PEER_ORG1_COMPOSE_PATH $OLD_NETWORK_NAME $NETWORK_NAME
+replaceNetworkName $ORDERER0_COMPOSE_PATH $OLD_NETWORK_NAME $NETWORK_NAME
+# Org2
+replaceOrgFunc ${ORG2_HOSTNAME} ${SERVICE_ORG2_COMPOSE_PATH} ${PEER_ORG2_COMPOSE_PATH} ${ORDERER1_COMPOSE_PATH}
+replaceNetworkName $SERVICE_ORG2_COMPOSE_PATH $OLD_NETWORK_NAME $NETWORK_NAME
+replaceNetworkName $PEER_ORG2_COMPOSE_PATH $OLD_NETWORK_NAME $NETWORK_NAME
+replaceNetworkName $ORDERER1_COMPOSE_PATH $OLD_NETWORK_NAME $NETWORK_NAME
+# Org3
+replaceOrgFunc ${ORG3_HOSTNAME} ${SERVICE_ORG3_COMPOSE_PATH} ${PEER_ORG3_COMPOSE_PATH} ${ORDERER2_COMPOSE_PATH}
+replaceNetworkName $SERVICE_ORG3_COMPOSE_PATH $OLD_NETWORK_NAME $NETWORK_NAME
+replaceNetworkName $PEER_ORG3_COMPOSE_PATH $OLD_NETWORK_NAME $NETWORK_NAME
+replaceNetworkName $ORDERER2_COMPOSE_PATH $OLD_NETWORK_NAME $NETWORK_NAME
 
 if [ "$ARCH" == "Darwin" ]; then
   rm */**.ymlt
