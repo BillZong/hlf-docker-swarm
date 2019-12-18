d5fce4b168db8eb081ba6418757a8890098196e6
BillZong <billzong@163.com>
2019-12-18 17:35:37 +0800

    download neccessary binary

--- network/move_crypto.sh
+++ network/move_crypto.sh
@@ -20,4 +20,28 @@ cd -
 cp -R crypto-config ${VOLUMES_DIR}/certs/
 cp -R config ${VOLUMES_DIR}/certs/
 cp -R ../chaincodes/* ${VOLUMES_DIR}/chaincode/
+
+downloadBinary() {
+    binaryFileName=hyperledger-fabric-$FABRIC_IMAGE_ARCH-$FABRIC_IMAGE_VERSION.tar.gz
+    if [ ! -f ../../$binaryFileName ]; then
+        curl -LO https://github.com/hyperledger/fabric/releases/download/$FABRIC_VERSION/$binaryFileName
+        mv $binaryFileName ../../
+    fi
+    tar -zxf ../../$binaryFileName
+}
+
+# download binary neccessary
+if [ ! -f bin/configtxgen ]; then
+    downloadBinary
+else
+    # check version
+    chmod +x bin/configtxgen
+    ver=`./bin/configtxgen --version | grep $FABRIC_IMAGE_VERSION`
+    # echo $ver
+    if [ "$ver" = "" ]; then
+        downloadBinary
+    fi
+fi
+
+chmod +x bin/*
 cp -R bin/* ${VOLUMES_DIR}/bin/
